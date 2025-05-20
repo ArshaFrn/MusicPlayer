c1b394e935b4ae0e1577cc32f1973e8470f2c5f2
Arsha Frn <83229386+ZvantablackZ@users.noreply.github.com>
2025-05-20 21:58:54 +0330

    Commit --Arsha

--- lib/main.dart
+++ lib/main.dart
@@ -55,16 +55,44 @@ class _MyHomePageState extends State<MyHomePage> {
           Center(
             child: Column(
               children: [
-                const SizedBox(height: 110),
-                const Text(
-                  'Hertz',
-                  style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+                const SizedBox(height: 88),
+                ShaderMask(
+                  shaderCallback: (Rect bounds) {
+                    return const LinearGradient(
+                      colors: [Color(0xFFFF5AF7), Color(0xFF8456FF)], // Pink to Purple
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ).createShader(bounds);
+                  },
+                  child: const Text(
+                    'Hertz',
+                    style: TextStyle(
+                      fontSize: 52,
+                      fontWeight: FontWeight.w300,
+                      fontFamily: 'Orbitron',
+                      color: Colors.white,
+                      letterSpacing: 5,
+                      shadows: [
+                        Shadow(
+                          blurRadius: 24,
+                          color: Color(0xFFFF5AF7), // Neon pink glow
+                          offset: Offset(0, 0),
+                        ),
+                        Shadow(
+                          blurRadius: 48,
+                          color: Color(0xFF8456FF), // Neon purple glow
+                          offset: Offset(0, 0),
+                        ),
+                        Shadow(
+                          blurRadius: 2,
+                          color: Colors.white,
+                          offset: Offset(0, 0),
+                        ),
+                      ],
+                    )
                   ),
                 ),
-                Image.asset('assets/images/logoTP.png', width: 150, height: 130),
+                  Image.asset('assets/images/logoTPP.png', width: 120, height: 90),
               ],
             ),
           ),
