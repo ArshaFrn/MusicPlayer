c085c16bb1565afe01993b3dc99b936b6785f874
Arsha Frn <83229386+ZvantablackZ@users.noreply.github.com>
2025-06-05 12:41:44 +0330

    signUp dart file made , signUp functionality done

--- lib/TcpClient.dart
+++ lib/TcpClient.dart
@@ -12,32 +12,26 @@ class TcpClient {
     String email,
   ) async {
     try {
-      // Establish connection to the server
       final socket = await Socket.connect(serverAddress, serverPort);
       print(
         'Connected to: ${socket.remoteAddress.address}:${socket.remotePort}',
       );
 
-      // Prepare the JSON request
       final request = {
         "Request": "signUpCheck",
         "Payload": {"username": username, "email": email},
       };
 
-      // Send the request to the server
       socket.write(jsonEncode(request));
       print('Request sent: $request');
 
-      // Listen for the server response
       final response =
           await socket.cast<List<int>>().transform(const Utf8Decoder()).join();
 
       print('Raw response received: $response');
 
-      // Close the socket
       socket.close();
 
-      // Handle empty or corrupted responses
       if (response.isEmpty) {
         print('Error: Empty response from server');
         return {"status": "error", "message": "Empty response from server"};
