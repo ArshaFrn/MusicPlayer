fb88db5ca44319bedbca1dc41ef35e4e0de9a252
Arsha Frn <83229386+ZvantablackZ@users.noreply.github.com>
2025-06-14 00:20:14 +0330

    added the singleton pattern for Application.dart --Arsha

--- lib/Application.dart
+++ lib/Application.dart
@@ -1,3 +1,30 @@
-class Application{
+import 'Music.dart';
+import 'User.dart';
+
+// Applicaation Flow Controller
+class Application {
+  static final Application _instance = Application._privateConstructor();
+
+  Application._privateConstructor();
+
+  static Application get instance => _instance;
+
+  bool likeMusic(User user, Music music) {
+    if (user.likedSongs.contains(music)) {
+      unlikeMusic(user, music);
+      return false;
+    }
+    user.likedSongs.add(music);
+    return true;
+  }
+
+  bool unlikeMusic(User user, Music music) {
+    if (!user.likedSongs.contains(music)) {
+      likeMusic(user, music);
+      return false;
+    }
+    user.likedSongs.remove(music);
+    return true;
+  }
 
 }
